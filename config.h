3aa3c055a06141f43be181ff8a880c6ec03563bc
Russell Cloak <russcloak@gmail.com>
2016-07-24 21:26:05 -0700

    Set MINTHROTTLE for the simonk flashed ESCs

--- config.h
+++ config.h
@@ -58,9 +58,9 @@
        This is the minimum value that allow motors to run at a idle speed  */
     //#define MINTHROTTLE 1300 // for Turnigy Plush ESCs 10A
     //#define MINTHROTTLE 1120 // for Super Simple ESCs 10A
-    //#define MINTHROTTLE 1064 // special ESC (simonk)
+    #define MINTHROTTLE 1064 // special ESC (simonk)
     //#define MINTHROTTLE 1050 // for brushed ESCs like ladybird
-    #define MINTHROTTLE 1220 // (*) (**)
+    //#define MINTHROTTLE 1220 // (*) (**)
 
   /****************************    Motor maxthrottle    *******************************/
     /* this is the maximum value for the ESCs at full power, this value can be increased up to 2000 */
